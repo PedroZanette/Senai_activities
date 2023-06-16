b174823b482b3174d789e641e05678012610a050
PEDRO HENRIQUE NUNES ZANETTE <126621567+PedroZanette@users.noreply.github.com>
2023-06-16 10:03:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-function converte(){
-    var temperaturaInicial = Number(document.getElementById("campo").value)
+function calcu(){
+    var calculo = Number(document.getElementById("valore").value)
+    var calculo2 = Number(document.getElementById("valore2").value)
     var resultado = 0
-    if (document.getElementById("cf").checked){
-         resultado = (temperaturaInicial*1.8) + 32
-    }
-    else if(document.getElementById("fc").checked){
-         resultado = (temperaturaInicial - 32) / 1.8
-    }
-    else if(document.getElementById("ck").checked){
-        resultado = (temperaturaInicial + 273.15)
-    }
-    else if(document.getElementById("kc").checked){
-        resultado = (temperaturaInicial - 273.15)
-    }
-    else if(document.getElementById("kf").checked){
-        resultado = ((9*(temperaturaInicial - 273.15)) / 5) + 32
-    }
-    else if(document.getElementById("fk").checked){
-        resultado = 5*(temperaturaInicial - 32) / 9 + 273.15
-    }
-    else{
-        resultado = "Você me parece pobre"
-        alert(resultado)
-    }
-   
-    document.getElementById("resultado").innerHTML = resultado
+    if (document.getElementById("pc").checked){
+        resultado = calculo - calculo2
+        if (resultado == 0){
+            resultado = "O valor foi pago com sucesso!"
+        }
+        else if (resultado >= 0){
+            resultado = "Falta pagar " + resultado + "$"
+        }
+        else if (resultado <= 0){
+            resultado = "Troco de " + resultado + "$"
+   } 
+   }
+   else if(document.getElementById("dl").checked){
+        resultado = calculo / calculo2 
+        resultado = "O veículo faz " + Math.floor(resultado, -2) + " por litro"
+   }
+   else if(document.getElementById("ni").checked){     
+        while (calculo >= 0) {
+         if (calculo % 2 == 0) {
+          resultado = resultado + 1
+          calculo = calculo - 1
+          }
+         }
+        resultado = "Seu número contém " + resultado
+   }
+   else if(document.getElementById("np").checked){
+       resultado = (temperaturaInicial - 273.15)
+   }
+   else{
+    resultado = "Insira um valor válido"
+    alert(resultado)
+   }
+
+     document.getElementById("resultado").innerHTML = resultado
 }
