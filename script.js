f2856968d236d4ebf0f557b61888af0a25270094
PEDRO HENRIQUE NUNES ZANETTE <126621567+PedroZanette@users.noreply.github.com>
2023-08-11 09:50:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-function converte(){
-    var temperaturaInicial = Number(document.getElementById("campo").value)
-    var resultado = 0
-    if (document.getElementById("cf").checked){
-         resultado = (temperaturaInicial*1.8) + 32
+var resultado = 0
+
+function anoNovo(){
+    let i = 10
+    while(i > 0){
+        resultado = i
+        i--
+        console.log(resultado)
     }
-    else if(document.getElementById("fc").checked){
-         resultado = (temperaturaInicial - 32) / 1.8
-    }
-    else if(document.getElementById("ck").checked){
-        resultado = (temperaturaInicial + 273.15)
-    }
-    else if(document.getElementById("kc").checked){
-        resultado = (temperaturaInicial - 273.15)
-    }
-    else if(document.getElementById("kf").checked){
-        resultado = ((9*(temperaturaInicial - 273.15)) / 5) + 32
-    }
-    else if(document.getElementById("fk").checked){
-        resultado = 5*(temperaturaInicial - 32) / 9 + 273.15
-    }
-    else{
-        resultado = "Você me parece pobre"
-        alert(resultado)
-    }
-   
+    resultado = "FELIZ ANO NOVO"
+    console.log(resultado)
     document.getElementById("resultado").innerHTML = resultado
+}
+function tabuada(){
+    var numer = (document.getElementById("numero").value)
+    
+    for(let i = 1;i <= 10;i++){
+      resultado = numer * i
+      console.log(numer, " x " , i, " = ", resultado)
+     
+    }
+
+document.getElementById("resultado").innerHTML = resultado
 }
