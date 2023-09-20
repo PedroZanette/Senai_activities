2fa42dfa8b40bd9b762e493be305bdc4eb033189
PEDRO HENRIQUE NUNES ZANETTE <126621567+PedroZanette@users.noreply.github.com>
2023-09-20 12:41:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,30 +3,53 @@ const campoSenha = document.getElementById("password");
 const campoNovoLogin = document.getElementById("novoLogin");
 const campoNovaSenha = document.getElementById("novaSenha");
 const campoRepSenha = document.getElementById("repSenha");
-let usuarios = [];
 
-function login(){
-   let login = campoLogin.value;
-   let senha = campoSenha.value;
-   let mensagem = "Usuário ou senha incorreta!";
-   for(let usuario of usuarios){
-        if(usuario.login == login && usuario.senha == senha){
-            mensagem = "parabéns, você logou!";
-            break;
+function login() {
+    let login = campoLogin.value;
+    let senha = campoSenha.value;
+    let mensagem = "Usuário ou senha incorreta!";
+    let bancoDeDados = JSON.parse(localStorage.getItem("bancoDeDados"));
+
+    if (bancoDeDados == null) {
+        mensagem = "Nenhum usuário cadastrado até o momento";
+    } else {
+        // Lógica para verificar as credenciais de login
+        for (let usuario of bancoDeDados) {
+            if (usuario.login == login && usuario.senha == senha) {
+                mensagem = "Parabéns, você logou!";
+                localStorage.setItem("logado", JSON.stringify(usuario));
+                window.location.href = "./logado/home.html"
+                break;
+            }
         }
+
     }
     alert(mensagem)
+
 }
 
-function cadastra(){
+function cadastra() {
+    if(verificaSeExiste(campoNovoLogin.value, bancoDeDados)){
+        alert("Já existe um usuário BOCÓ!")
+    }
     if (campoNovaSenha.value == campoRepSenha.value) {
         const usuario = {
-         login: campoNovoLogin.value,
-         senha: campoNovaSenha.value
+            login: campoNovoLogin.value,
+            senha: campoNovaSenha.value
+        };
+        let bancoDeDados = JSON.parse(localStorage.getItem("bancoDeDados"));
+        if (bancoDeDados == null) {
+            bancoDeDados = [];
         }
-     usuarios.push(usuario);
-     alert("Usuarío cadastrado com sucesso")
-    }else{
-    alert("As senhas são diferentes!");
-  }
+        bancoDeDados.push(usuario);
+        localStorage.setItem("bancoDeDados", JSON.stringify(bancoDeDados));
+
+    } else {
+        alert("As senhas são diferentes!");
+    }
+    alert("Usuário cadastrado com sucesso!")
+}
+
+function verificaSeExiste(){
+    
 }
