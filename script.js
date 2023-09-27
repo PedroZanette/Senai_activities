79bd448f77cccb4a276214bfc9b628dc48fd2787
PEDRO HENRIQUE NUNES ZANETTE <126621567+PedroZanette@users.noreply.github.com>
2023-09-27 12:42:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function login() {
             if (usuario.login == login && usuario.senha == senha) {
                 mensagem = "Parabéns, você logou!";
                 localStorage.setItem("logado", JSON.stringify(usuario));
-                window.location.href = "home.html"
+                window.location.href = "./logado/home.html"
                 break;
             }
         }
